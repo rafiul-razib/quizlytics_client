d080924fd58f1758e5269c23d5696980635f3651
rafiul-razib <rafiul.razib@gmail.com>
2026-01-02 19:12:35 +0600

    google cloud console credentials updated

--- src/app/api/create-payment-intent/route.js
+++ src/app/api/create-payment-intent/route.js
@@ -1,27 +1,27 @@
-import {NextResponse} from "next/server";
-import Stripe from "stripe";
+// import {NextResponse} from "next/server";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export async function POST(request) {
-  try {
-    const body = await request.json();
-    const {prices, email, userName} = body;
+// export async function POST(request) {
+//   try {
+//     const body = await request.json();
+//     const {prices, email, userName} = body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: prices,
-      currency: "usd",
-      receipt_email: email,
-      metadata: {userName, email},
-      automatic_payment_methods: {enabled: true},
-    });
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: prices,
+//       currency: "usd",
+//       receipt_email: email,
+//       metadata: {userName, email},
+//       automatic_payment_methods: {enabled: true},
+//     });
 
-    return NextResponse.json({clientSecret: paymentIntent.client_secret});
-  } catch (error) {
-    console.error("Internal Error:", error);
-    return NextResponse.json(
-      {error: `Internal Server Error: ${error}`},
-      {status: 500}
-    );
-  }
-}
+//     return NextResponse.json({clientSecret: paymentIntent.client_secret});
+//   } catch (error) {
+//     console.error("Internal Error:", error);
+//     return NextResponse.json(
+//       {error: `Internal Server Error: ${error}`},
+//       {status: 500}
+//     );
+//   }
+// }
